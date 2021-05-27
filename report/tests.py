a189850454e8bf234c579d10237169a4b0ca7ddc
lazaro <lazlemlop@gmail.com>
2021-05-26 23:46:09 -0400

    commit

--- report/tests.py
+++ report/tests.py
@@ -4,26 +4,14 @@ from django.test import TestCase
 
 
 
+import os
+
+print(os.environ.get('SECRET_KEY'))
 
 
-
-lst_string_nums = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
-def convert_num_to_letter(number):
-    if number in range(1, 11):
-        number = lst_string_nums[number + 1]
-        return number
-
-
-
-    
     
 
 
 
 
     
-
-
-if d.time and d.additional_comments and not d.visitor_front_gate and not d.resident_front_gate and not d.exit_front_gate and not d.entrace_back_gate 
-                        and not d.exit_back_gate and not d.gym and not d.pool and not d.bascketball_court and not d.tennis_court and not d.club_house and not d.cameras_not_working
-                        and not d.gates_not_working and not d.gate_arms_not_working 
