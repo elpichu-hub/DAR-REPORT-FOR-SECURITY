27aba1ac671c6a554d6b66d2e4c1955c84f679cd
lazaro <lazlemlop@gmail.com>
2021-05-28 23:28:35 -0400

    somechange

--- report/tests2.py
+++ report/tests2.py
@@ -8,3 +8,12 @@ print(my_time)
 
 time = datetime.datetime(2021, 10, 30, 23)
 print(time)
+
+def test(*args):
+    for arg in args:
+        if arg == time:
+            print(f'date and time is {time}')
+        else:
+            print('no')
+
+test(time, utc_now)
